c2b6248a71e1d6cf3e26e0ab537929197390e24f
emirhan zengin <107498774+emirhanzengindev@users.noreply.github.com>
2025-10-02 15:41:11 +0300

    Update Transaction.js

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,11 +1,9 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../config/database.js";
-// Diğer importlar (User, Category) burada olabilir veya relations dosyasına taşınmış olmalıdır.
-// Hata çözümü için bu dosyanın sadece bir kez export yapması önemlidir.
+
 
 export const Transaction = sequelize.define('Transaction', {
-    // Model Sütunları
-    // ...
+
     id: {
         type: DataTypes.UUID,
         defaultValue: DataTypes.UUIDV4,
@@ -27,12 +25,9 @@ export const Transaction = sequelize.define('Transaction', {
         type: DataTypes.ENUM('income', 'expense'),
         allowNull: false,
     },
-    // Foreign Keys: user_id ve category_id (associations.js'te eklenecek)
+
 }, {
     tableName: 'transactions',
-    // Eğer User ve Category import edildiyse, ilişki kodu da buradaydı
-    // Ancak en temiz çözüm: İlişki kodlarını (Transaction.belongsTo) buradan KALDIRIN.
+
 });
 
-// Eğer ilişkileri associations.js'e taşıdıysanız, bu kadar.
-// TEKRARLI export yapmadığınızdan emin olun.
